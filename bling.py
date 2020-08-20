03d80eac26f3a214f49ecb6176c2a8948f9ad102
Danilo Guedes <danilotiba@gmail.com>
2020-08-20 14:07:33 -0300

    Comitando meus primeiros projetos

---
+++ bling.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append('C:/Users/dan_g/Envs/myprojects/Lib/site-packages')
+
+from bs4 import BeautifulSoup
+import requests
+import selenium
+from selenium import webdriver
+import time
+from senhas import login_bling, senha_bling, site_bling
+
+
+# site = requests.get('https://www.bling.com.br/login')
+# print(site.content)
+
+#abrindo o site a acessando na minha conta
+def relatorio_de_vendas_diaras():
+    path_para_chrome_driver = 'C:/Users/dan_g/Desktop/programação/Python/myprojects/chromedriver.exe'
+    browser = webdriver.Chrome(executable_path = path_para_chrome_driver)
+    browser.get(site_bling)
+    browser.find_element_by_id('username').clear()
+    browser.find_element_by_id('username').send_keys(login_bling)
+    browser.find_element_by_id('senha').clear()
+    browser.find_element_by_id('senha').send_keys(senha_bling)
+    browser.find_element_by_name('enviar').click()
+    time.sleep(2)
+    browser.get('https://www.bling.com.br/relatorios.php?id=3&nome=Vendas') #acessando a tela do relatório de vendas
+    time.sleep(2)
+    browser.find_element_by_xpath('//*[@id="resultado"]/ul[3]/li[2]').click()  # escolhendo o relatório de vendas
+    time.sleep(2)
+    browser.find_element_by_xpath('//*[@id="periodoPesq"]').click()
+    time.sleep(0.5)
+    browser.find_element_by_xpath('//*[@id="periodoPesq"]/option[2]').click() # escolhendo o período como "do dia"
+    time.sleep(2)
+    browser.find_element_by_xpath('//*[@id="campo1"]').click()
+    time.sleep(0.5)
+    browser.find_element_by_xpath('//*[@id="campo1"]/option[2]').click() # escolhendo agrupar por "produto"
+    time.sleep(2)
+    browser.find_element_by_id('btnVisualizar').click() #clicando em visualizar
+    time.sleep(2)
+    browser.find_element_by_xpath('//*[@id="exportarRelatorio"]').click() # exportando para Excel
+    time.sleep(2)
+    browser.find_element_by_xpath('/html/body/div[10]/div[3]/div/button[1]').click()
+    time.sleep(2)
+    browser.find_element_by_xpath ('//*[@id="menu-acoes"]/li[5]/a').click()
+
+if __name__ == '__main__':
+    relatorio_de_vendas_diaras()
---
+++ organizador_de_arquivos_por_ext.py
@@ -0,0 +1,78 @@
+""" Módulo para treinar com a biblioteca OS"""
+
+
+import os, os.path
+
+
+def listando_arquivos():
+    global path_escolhido, lista_geral, so_arquivos, so_dir
+    path_escolhido = str(input('preencha com o caminho do diretório escolhido >>  '))
+
+    lista_geral = os.listdir(path_escolhido)
+
+    #descobrindo o que são arquivos e o que são diretórios
+    so_arquivos = [i.name for i in os.scandir(path_escolhido) if i.is_file()]
+    so_dir = [i.name for i in os.scandir(path_escolhido) if i.is_dir()]
+
+    print(f'Seu diretório escolhido tem {len(lista_geral)} arquivos, sendo {len(so_arquivos)} arquivos e {len(so_dir)} diretórios')
+
+    #posicionando o script no diretorio escolhido para futura interação
+    os.chdir(path_escolhido)
+    print("__**__**" * 10)
+
+def relacionar_arquivos():
+    while True:
+        resposta = input('Você gostaria de listar seus arquivos e diretórios? (Y/N)').upper()
+        if resposta not in ('Y', 'N'):
+            print('você precisa escolher uma das opções possíveis (Y/N)')
+        elif resposta == 'Y':
+            for i in lista_geral:
+                print(i)
+            break
+        else:
+            break
+
+def finding_extentions():
+    global lista_extensoes
+    lista_extensoes = []
+    for i in so_arquivos:
+        ext = i.split('.')[-1].lower()
+        if ext not in lista_extensoes:
+        #lista_extencoes.append(i.split('.')[-1])
+            lista_extensoes.append(ext)
+    print(f'>>>você tem arquivos em {len(lista_extensoes)} extenções diferentes')
+    print(f'>>>sendo elas {lista_extensoes}')
+    print("__**__**" * 10)
+
+
+def deseja_organizar_arquivos():
+    while True:
+        resposta = input('Você gostaria de ORGANIZAR seus arquivos em diretórios por extenção? (Y/N)').upper()
+        if resposta not in ('Y', 'N'):
+            print('você precisa escolher uma das opções possíveis (Y/N)')
+        elif resposta == 'Y':
+            dir_atual = os.getcwd()  #<class 'str'>
+            for i in lista_extensoes: #criando 1 pasta para cada tipo de extencao
+                nova_dir = dir_atual + "/" + i
+                os.mkdir(nova_dir)
+            for i in so_arquivos: #movendo os arquivos para as novas pastas
+                atual = os.getcwd() + "/" + i
+                destino = os.getcwd() + "/" + i.split('.')[-1] + "/" + i
+                os.rename(atual, destino)
+            print(f'>>> seus arquivos foram ORGANIZADOS!!')
+            break
+        else:
+            break
+
+
+if __name__ == "__main__":
+    listando_arquivos()
+    relacionar_arquivos()
+    finding_extentions()
+    deseja_organizar_arquivos()
+
+    #print(f'seu local atual é {os.getcwd()}')
+    #print(f'o diretório escolhido é {path_escolhido}')
+
+
+# c:/users/dan_g/teste_desktop
